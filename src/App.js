49516e472a42445f53c3166939735c1eb89ce337
Anushkajoshii <anushkajoshi020503@gmail.com>
2024-06-26 21:24:54 +0530

    change 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import {
-  withAuthenticator,
   Button,
   Heading,
-  Image,
-  View,
   Card,
+  View,
+  withAuthenticator,
+  Image,
 } from "@aws-amplify/ui-react";
 
-function App({signOut}){
-  return (
-    <View className='App'>
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}> We have Auth</Heading>
+
+
+function App ({signOut}){
+  return(
+    <View className="App">
+      <Card> 
+        <Image>
+          <img src={logo} alt="logo" />
+        </Image>
+        <Heading level={1}>Welcome to the Amplify UI React</Heading>
       </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
+      <Button>signOut</Button>
+    </View> 
+  )
 }
 export default withAuthenticator(App);
